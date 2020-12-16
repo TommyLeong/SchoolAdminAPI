5e5f6808d7c17ba75083839d2ea56add4f27eb4f
Tommy Leong <tommymoleong@gmail.com>
2020-12-16 23:08:13 +0800

    - Update method to GET - findAll existing teacher in DB

--- src/controllers/GenerateReportController.js
+++ src/controllers/GenerateReportController.js
@@ -1,12 +1,26 @@
 import Express from 'express';
 import { OK } from 'http-status-codes';
+import Teacher from '../models/Teacher'
+import Student from '../models/Student'
+import Subject from '../models/Subject'
+import Class from '../models/Class'
 
 const GenerateReportController = Express.Router();
 
 const generateReportHandler = async (req, res) => {
+  const results = await Teacher.findAll({raw : true});
+  console.log(results)
+
+  const teachersName = []
+
+  console.log(results.every(teacher => {
+    teachersName.push(teacher.name)
+    console.log(teacher)
+  }));
+
   return res.sendStatus(200);
 }
 
-GenerateReportController.post('/reports/workload', generateReportHandler);
+GenerateReportController.get('/reports/workload', generateReportHandler);
 
 export default GenerateReportController;
